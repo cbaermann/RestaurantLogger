9589a66d3538b0454eeeffffcd30b37b0974c4b7
Cody Baermann <cbaermann@comcast.net>
2020-06-04 14:24:42 -0700

    landing page being worked on

--- RestaurantLogger/client/src/views/Landing.jsx
+++ RestaurantLogger/client/src/views/Landing.jsx
@@ -1,7 +1,18 @@
 import React from 'react';
+import { Button } from '@material-ui/core';
+
+import '../style/Landing.css'
 
 export default () => {
     return(
-        <a href="/restaurant">Go to site</a>
+        <>
+        <div className="container">
+            <img className="landingImage" src={require('../image/logo.png')}></img><br/>
+            <Button className="siteButton" href="/restaurant"
+            color="default"
+            variant="contained">Go To Site</Button>
+            
+        </div>
+        </>
     )
 }
