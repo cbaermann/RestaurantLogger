d0503fc8902f2f29ec62eb4005c99c60fb20cc01
Cody Baermann <cbaermann@comcast.net>
2020-06-04 10:30:30 -0700

    delete button fixed

--- RestaurantLogger/client/src/components/DeleteButton.jsx
+++ RestaurantLogger/client/src/components/DeleteButton.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import axios from 'axios';
-import { Button } from "@material-ui/core";
+import { Button } from '@material-ui/core'
+
 
 export default props => {
-    const { restaurantId, successCallback} = props;
+    const { restaurantId, successCallback } = props;
 
     const deleteRestaurant = e => {
-        axios.delete('/http://localhost:8000/api/restaurant/' + restaurantId)
+        axios.delete('http://localhost:8000/api/restaurant/' + restaurantId)
             .then(res => {
                 successCallback();
             })
