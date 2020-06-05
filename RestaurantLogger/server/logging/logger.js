24d81b7a6b04d0ad1cccadd7d73a28dafee9f218
RJ <52873153+tupleHunden@users.noreply.github.com>
2020-06-05 15:52:59 -0400

    Updated the Logger to provide request type.

--- RestaurantLogger/server/logging/logger.js
+++ RestaurantLogger/server/logging/logger.js
@@ -1,7 +1,7 @@
 const moment = require('moment');
 
 const logger = (req, res, next) => {
-    console.log(`${req.protocol}://${req.get('host')}${req.originalUrl}: ${moment().format('MMMM Do YYYY, h:mm:ss a')}`);
+    console.log(`${req.protocol}://${req.get('host')}${req.originalUrl} ${req.method}: ${moment().format('MMMM Do YYYY, h:mm:ss a')}`);
     next();
 };
 
