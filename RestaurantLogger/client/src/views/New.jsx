7355740c976314ec2faea591da49e897f066e132
RJ <52873153+tupleHunden@users.noreply.github.com>
2020-06-05 11:21:28 -0400

    Added a Price Range category.

--- RestaurantLogger/client/src/views/New.jsx
+++ RestaurantLogger/client/src/views/New.jsx
@@ -21,7 +21,8 @@ export default () => {
             initialName=""
             initialLocation=""
             initialDescription=""
-            initialFoodType="" />
+            initialFoodType=""
+            initialPriceRange=""/>
         </div>
     )
 }
