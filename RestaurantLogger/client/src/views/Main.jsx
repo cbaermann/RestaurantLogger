193b760b42f44d1cd11f48c92d93dd0e6f03a76a
Cody Baermann <cbaermann@comcast.net>
2020-06-05 07:56:32 -0700

    push

--- RestaurantLogger/client/src/views/Main.jsx
+++ RestaurantLogger/client/src/views/Main.jsx
@@ -1,6 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { Button } from '@material-ui/core';
 import RestaurantList from '../components/RestaurantList';
+
+
+const styles = {
+    addButton: {
+        backgroundColor: "hotPink"
+    }
+}
 
 export default() => {
     const [restaurant, setRestaurant] = useState([]);
@@ -21,7 +29,8 @@ export default() => {
     return(
         <div>
             <h1>Your Favorite Restaurants</h1>
-            <a href="/new">Add a new restaurant</a>
+            {/* <a href="/new">Add a new restaurant</a> */}
+            <Button style={styles.addButton} variant="contained" href='/new'>Add a new restaurant</Button>
             {loaded && <RestaurantList restaurant={restaurant} removeFromDom={removeFromDom} />}
         </div>
     )
