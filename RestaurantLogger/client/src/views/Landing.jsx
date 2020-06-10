b9201b3d8b9daec03a46712e4d2c4b8688410408
Cody Baermann <cbaermann@comcast.net>
2020-06-10 14:56:48 -0700

    material icon github, linked to repository

--- RestaurantLogger/client/src/views/Landing.jsx
+++ RestaurantLogger/client/src/views/Landing.jsx
@@ -1,12 +1,22 @@
 import React from 'react';
 import { Button } from '@material-ui/core';
+import GitHubIcon from '@material-ui/icons/GitHub';
 
 import '../style/Landing.css'
+
+const styles = {
+    githubButton: {
+        position: "absolute",
+        top: 0,
+        right: 30,
+    }
+}
 
 export default () => {
     return(
         <>
         <div className="container">
+            <Button style={styles.githubButton} className="githubButton" href="https://github.com/cbaermann/RestaurantLogger"><GitHubIcon fontSize="large" style={{color: "white"}}/></Button><br/>
             <img className="landingImage" src={require('../image/logo.png')} alt={'application logo'}/><br/>
             <Button className="siteButton" href="/restaurant"
             color="default"
